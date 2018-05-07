babe7c4cdbd8ed8b9d4eee060276fd0d389396f3
Zackyy1 <38585862+Zackyy1@users.noreply.github.com>
2018-05-07 17:40:36 +0300

    Update OfferMaker.py

--- OfferMaker.py
+++ OfferMaker.py
@@ -202,7 +202,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://seljankatest.herokuapp.com/' + token)
+    bot.set_webhook(url='https://seljankaadmaker.herokuapp.com/' + token)
     return "!", 200
 
 
